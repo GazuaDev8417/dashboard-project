d1c47435fcb7f456b71096fe0f34836cad79449d
GazuaDev8417 <francadasilvaflamarion>
2026-07-11 01:20:31 -0300

    task: explnation of elements

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,45 @@
-import { Bell } from "lucide-react"
+import { Bell, Search } from "lucide-react"
 
 
 export default function Header(){
     return(
         <header className="flex h-16 items-center justify-between border-b border-slate-200 bg-white px-6">
-            {/* left side */}
-            <div>
-                <h1 className="text-lg font-semibold text-slate-800">
-                    Dashboard SaaS
-                </h1>
+            {/* Search */}
+            <div className="relative w-80">
+                <Search
+                    size={18}
+                    className="absolute left-3 top-1/2 -translate-y-1/2 text-slate-400"/>
+                
+                <input
+                    type='text'
+                    placeholder="Search..."
+                    className="w-full rounded-lg border border-slate-300 py-2 pl-10 pr-4 outline-none transition focus:border-blue-500"/>
             </div>
+            
 
             {/* right side */}
-            <div className="flex items-center gap-4">
-                <Bell
-                    size={20}
-                    className="cursor-pointer text-slate-600 hover:text-slate-900"/>
+            <div className="flex items-center gap-5">
+                <button className="relative rounded-full p-2 transition hover:bg-slate-100">
+                    <Bell size={20}/>
 
-                <div className="h-10 w-10 rounded-full bg-slate-300"/>
+                    <span className="absolute right-1 top-1 h-2 w-2 rounded-full bg-red-500"/>
+                </button>
+
+                <div className="flex items-center gap-3">
+                    <div className="flex h-10 w-10 items-center justify-center rounded-full bg-blue-600 font-semibold text-white">
+                        FF
+                    </div>
+
+                    <div>
+                        <p className="text-sm font-semibold text-slate-800">
+                            Flamarion França
+                        </p>
+
+                        <p className="text-xs text-slate-500">
+                            Full Stack Developer
+                        </p>
+                    </div>
+                </div>
             </div>
         </header>
     )
--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,12 +1,42 @@
 import RecentOrdersTable from "@/components/dashboard/RecentOrdersTable"
 import DashboardWidget from "@/components/dashboard/DashboardWidget"
 import SalesChart from "@/components/dashboard/SalesChart"
+import StatCard from "@/components/ui/StatCard"
+
+
+
+const statistics = [
+    {
+        title: 'Revenue',
+        value: '$4, 500'
+    },
+    {
+        title: 'Customer',
+        value: '1,245'
+    },
+    {
+        title: 'Orders',
+        value: '325'
+    },
+    {
+        title: 'Products',
+        value: '98'
+    }
+]
 
 
 
 export default function DashboardPage(){
     return(
         <section className="space-y-9">
+            <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-4">
+                {statistics.map((stat) => (
+                    <StatCard
+                        key={stat.title}
+                        title={stat.title}
+                        value={stat.value}/>
+                ))}
+            </div>
             <DashboardWidget title="Revenue Overview">
                 <SalesChart/>
             </DashboardWidget>
